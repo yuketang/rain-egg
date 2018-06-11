38350b83f7ffc9e823fa722d1691b274723f2da3
mahui <mahui@xuetangx.com>
2018-06-11 09:52:30 +0800

    add i18n config

--- config/config.default.js
+++ config/config.default.js
@@ -268,6 +268,16 @@ module.exports = appInfo => {
         },
     };
 
+  config.i18n = {
+    // 默认语言，默认 "en_US"
+    defaultLocale: 'en',
+    // URL 参数，默认 "locale"
+    queryField: 'locale',
+    // Cookie 记录的 key, 默认："locale"
+    // cookieField: 'locale',
+    // Cookie 默认 `1y` 一年后过期， 如果设置为 Number，则单位为 ms
+    // cookieMaxAge: '1y',
+  };
 
     config.HEADER_LANGUAGE = 'X-Language';
     return config;
